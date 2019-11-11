76927c33c37eb2c07f6788406ea34baf59e131f6
Tara DeGuzman <tara.deguzman323@gmail.com>
2019-11-11 11:50:57 -0800

    changes

--- main.js
+++ main.js
@@ -14,14 +14,14 @@ weather.temperature = {
 //dash data//
 
 const KELVIN = 273;
-const key = "82005d27a116c2880c8f0fcb866998a0";
+const key = "9ffa42dc1c134518653f98bd179a578b";
 
 //user facing (geo)//
 if ("geolocation" in navigator){
     navigator.geolocation.getCurrentPosition(setPosition, showError);
 } else {
     notifyElement.style.display = "block";
-    notifyElement.innerHTML = "<p> Browser doesn't support Location</p>";
+    notifyElement.innerHTML = `<p> Browser doesn't support Location</p>`;
 }
 
 function setPosition(position){
@@ -36,3 +36,28 @@ function setPosition(position){
      notifyElement.style.display = "block";
      notifyElement.innerHTML = `<p> ${error.message} </p>`;
  }
+
+ function getWeather (latitude, longitude) {
+     let api = `http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={9ffa42dc1c134518653f98bd179a578b}`;
+ }
+
+ fetch (api)
+    .then (function(response){
+        let data = response.json();
+        return data;
+    })
+    .then (function(data){
+        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+        weather.city = data.name;
+        weather.country = data.sys.country;
+    })
+    .then (function){
+        displayWeather ();
+    });
+}
+
+    function displayWeather(){
+        tempElement.innerHTML = `${weather.temperature.value}˚<span>F</span>`;
+        infoElement.innerHTML = weather.description;
+        locationElement.innerHTML = `${weather.city}, ${weather.country};`
+    }
