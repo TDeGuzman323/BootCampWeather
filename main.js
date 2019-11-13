3ca52b8943f21d3f2822fd7855c4bca5f4e71c9a
Tara DeGuzman <tara.deguzman323@gmail.com>
2019-11-12 19:06:52 -0800

    changes

--- main.js
+++ main.js
@@ -1,34 +1,17 @@
-var weather;
 
-var api = "http://api.openweathermap.org/data/2.5/weather?q="; 
-var apiKey = "&APPID=9ffa42dc1c134518653f98bd179a578b";
-var units = "&units=imperial";
-
-var input; 
-
-var button = document.getElementById("find")
-find.addEventListener("click", displayWeather)
-
-input = select ("city");
-
-function displayWeather(){
-var url = api + input.value() + apiKey + units;
-    loadJSON (url, getInfo);
+function getData (){
+    var cityLoc = $("#cityName").val();
+    var api = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=9ffa42dc1c134518653f98bd179a578b&units=imperial"; 
+    
+    $.getJSON (api,giveWeatherData);
 }
 
-function getInfo (info){
-    weather = info;
+
+function giveWeatherData (weatherInfo){
+    var cityLoc = weatherInfo.name;
+    var weatherTemp = weatherInfo.temp;
+    var weatherDescrip = weatherInfo.weather[0].description;
+    var countryLoc = weatherInfo.sys.country;
+    
+    $(".container").append("The Weather in " + cityLoc + "," + countryLoc + " is currently" + weatherTemp + " and" + weatherDescrip);
 }
- 
-
-
-// const degreesElement = document.querySelector(".degrees p");
-// const descripElement = document.querySelector(".temp-description p");
-// const locationElement = document.querySelector(".location p");
- 
-// const weather = {};
-// weather.temperature = {
-//     unit: "fahrenheit"
-
-
-
